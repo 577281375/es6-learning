a4b0ad10f96929138075a86a9e35b04a3107336f
songxue <Xsong@creams.io>
2019-01-25 18:11:38 +0800

    chore: es6 env build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HotModuleReplacementPlugin = require('hot-module-replacement');
+var branchName = require('current-git-branch');
 var path = require('path');
-module.exports = {
-    devtool: 'eval',
-    entry: [
-        './index.js'
-    ],
-    output: {
-        path: path.resolve(__dirname,'./dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-        overlay: true,
-        inline: true,
-    },
-    mode: 'development',
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        })
-    ]
-}
+
+module.exports = (env) => {
+    const config = {
+        devtool: 'eval',
+        entry: {
+            main: './index.js'
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'bundle.js'
+        },
+        devServer: {
+            contentBase: './dist',
+            hot: true,
+            overlay: true,
+            inline: true,
+        },
+        mode: 'development',
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                title: `${branchName()} demo test`,
+            })
+        ],
+        resolve: {
+            alias: {
+                'utils': path.join(__dirname, '/src/utils'),
+            }
+        }
+    }
+    return config;
+};
