7f90bf7a528c2255bd107416ebabbe1e4f681f5f
songxue <Xsong@creams.io>
2019-01-27 16:13:31 +0800

    chore: eslint && react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('mobx')
+/*
+ * @Author: songxue
+ * @Date: 2019-01-27 16:09:37
+ * @Last Modified by: songxue
+ * @Last Modified time: 2019-01-27 16:10:33
+ * @Module Name: ModuleName
+ */
+
+const a = 'dkfds';
+console.log(a);
